9d0505672989a761c24d532f32d7fda5d2b7045f
Joris Lans <s2978466@vuw.leidenuniv.nl>
2026-03-07 04:20:58 +0100

    Fixed sarsa and ran on/off policy experiment

--- Experiment.py
+++ Experiment.py
@@ -77,41 +77,42 @@ def experiment():
     optimal_episode_return = 83.67825468739042 # set the optimal return per episode you found in the DP assignment here
     
     #### Assignment 2: Effect of exploration
-    policy = 'egreedy'
-    epsilons = [0.03,0.1,0.3]
-    learning_rate = 0.1
-    backup = 'q'
-    Plot = LearningCurvePlot(title = 'Exploration: $\epsilon$-greedy versus softmax exploration')    
-    Plot.set_ylim(-100, 100) 
-    for epsilon in epsilons:    
-        print('Running {}-greedy with epsilon = {}'.format(backup,epsilon))    
-        learning_curve, timesteps = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-                                              gamma, policy, epsilon, temp, smoothing_window, plot, n, eval_interval)
-        Plot.add_curve(timesteps,learning_curve,label=r'$\epsilon$-greedy, $\epsilon $ = {}'.format(epsilon))    
-    policy = 'softmax'
-    temps = [0.01,0.1,1.0]
-    for temp in temps:
-        print('Running {} with softmax temperature = {}'.format(backup,temp))
-        learning_curve, timesteps = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-                                              gamma, policy, epsilon, temp, smoothing_window, plot, n, eval_interval)
-        Plot.add_curve(timesteps,learning_curve,label=r'softmax, $ \tau $ = {}'.format(temp))
-    Plot.add_hline(optimal_episode_return, label="DP optimum")
-    Plot.save('exploration.png')
+    # policy = 'egreedy'
+    # epsilons = [0.03,0.1,0.3]
+    # learning_rate = 0.1
+    # backup = 'q'
+    # Plot = LearningCurvePlot(title = 'Exploration: $\epsilon$-greedy versus softmax exploration')    
+    # Plot.set_ylim(-100, 100) 
+    # for epsilon in epsilons:    
+    #     print('Running {}-greedy with epsilon = {}'.format(backup,epsilon))    
+    #     learning_curve, timesteps = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
+    #                                           gamma, policy, epsilon, temp, smoothing_window, plot, n, eval_interval)
+    #     Plot.add_curve(timesteps,learning_curve,label=r'$\epsilon$-greedy, $\epsilon $ = {}'.format(epsilon))    
+    # policy = 'softmax'
+    # temps = [0.01,0.1,1.0]
+    # for temp in temps:
+    #     print('Running {} with softmax temperature = {}'.format(backup,temp))
+    #     learning_curve, timesteps = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
+    #                                           gamma, policy, epsilon, temp, smoothing_window, plot, n, eval_interval)
+    #     Plot.add_curve(timesteps,learning_curve,label=r'softmax, $ \tau $ = {}'.format(temp))
+    # Plot.add_hline(optimal_episode_return, label="DP optimum")
+    # Plot.save('exploration.png')
         
     # ###### Assignment 3: Q-learning versus SARSA
-    # policy = 'egreedy'
-    # epsilon = 0.1 # set epsilon back to original value 
-    # learning_rates = [0.03,0.1,0.3]
-    # backups = ['q','sarsa']
-    # Plot = LearningCurvePlot(title = 'Back-up: on-policy versus off-policy')    
-    # Plot.set_ylim(-100, 100) 
-    # for backup in backups:
-    #     for learning_rate in learning_rates:
-    #         learning_curve, timesteps = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-    #                                           gamma, policy, epsilon, temp, smoothing_window, plot, n, eval_interval)
-    #         Plot.add_curve(timesteps,learning_curve,label=r'{}, $\alpha$ = {} '.format(backup_labels[backup],learning_rate))
-    # Plot.add_hline(optimal_episode_return, label="DP optimum")
-    # Plot.save('on_off_policy.png')
+    policy = 'egreedy'
+    epsilon = 0.1 # set epsilon back to original value 
+    learning_rates = [0.03,0.1,0.3]
+    backups = ['q','sarsa']
+    Plot = LearningCurvePlot(title = 'Back-up: on-policy versus off-policy')    
+    Plot.set_ylim(-100, 100) 
+    for backup in backups:
+        for learning_rate in learning_rates:
+            print('Running {} with learning rate = {}'.format(backup,learning_rate))
+            learning_curve, timesteps = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
+                                              gamma, policy, epsilon, temp, smoothing_window, plot, n, eval_interval)
+            Plot.add_curve(timesteps,learning_curve,label=r'{}, $\alpha$ = {} '.format(backup_labels[backup],learning_rate))
+    Plot.add_hline(optimal_episode_return, label="DP optimum")
+    Plot.save('on_off_policy.png')
     
     # # ##### Assignment 4: Back-up depth
     # policy = 'egreedy'
--- SARSA.py
+++ SARSA.py
@@ -13,9 +13,11 @@ from Agent import BaseAgent
 class SarsaAgent(BaseAgent):
         
     def update(self,s,a,r,s_next,a_next,done):
-        max_a = np.max(self.Q_sa[s_next])
-        self.Q_sa[s, a] = self.Q_sa[s, a] + self.learning_rate * (
-                        r + self.gamma * self.Q_sa[s_next, a_next] - self.Q_sa[s, a])
+        if done:
+            target = r
+        else:
+            target = r + self.gamma * self.Q_sa[s_next, a_next]
+        self.Q_sa[s, a] += self.learning_rate * (target - self.Q_sa[s, a])
 
 
 def sarsa(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True, eval_interval=500):
@@ -28,40 +30,42 @@ def sarsa(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, tem
     eval_timesteps = []
     eval_returns = []
 
-    episodes = 10000
     # TO DO: Write your SARSA algorithm here!
-    for episode in range(episodes):
-        done = False
-        if episode % eval_interval == 0:
-            mean_return = pi.evaluate(env)
+    s = env.reset()
+    a = pi.select_action(s, policy, epsilon, temp)
+
+    for t in range(n_timesteps):
+        if t % eval_interval == 0:
+            mean_return = pi.evaluate(eval_env)
             eval_returns.append(mean_return)
-            eval_timesteps.append(episode)
-        s = env.reset()
-        while not done:
-            action = pi.select_action(s, policy, epsilon, temp)
-            s_next, r, done = env.step(action)
+            eval_timesteps.append(t)
+
+        s_next, r, done = env.step(a)
+
+        if done:
+            a_next = None
+        else:
             a_next = pi.select_action(s_next, policy, epsilon, temp)
-            pi.update(s, action, r, s_next, a_next, done)
+
+        pi.update(s, a, r, s_next, a_next, done)
+
+        if done:
+            s = env.reset()
+            a = pi.select_action(s, policy, epsilon, temp)
+        else:
             s = s_next
+            a = a_next
 
     if plot:
         s = eval_env.reset()
         for t in range(n_timesteps):
             a = pi.select_action(s, policy="greedy")  # sample random action
             s_next, r, done = eval_env.step(a)  # execute action in the environment
-            p_sas, r_sas = eval_env.model(s, a)
-            print(
-                "State {}, Action {}, Reward {}, Next state {}, Done {}, p(s'|s,a) {}, r(s,a,s') {}".format(
-                    s, a, r, s_next, done, p_sas, r_sas))
             eval_env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.5)  # display the environment
             if done:
                 s = eval_env.reset()
-                quit()
             else:
                 s = s_next
-
-    # if plot:
-    #    env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during SARSA execution
 
     return np.array(eval_returns), np.array(eval_timesteps) 
 

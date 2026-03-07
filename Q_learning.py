88c5027c3d70507ef342778d3a85051da80a90b4
RwikKamilya <rwikkamilya@gmail.com>
2026-03-07 07:51:26 +0100

    Fixed the terminal condition for Q-learning

--- Q_learning.py
+++ Q_learning.py
@@ -11,18 +11,20 @@ import numpy as np
 from Environment import StochasticWindyGridworld
 from Agent import BaseAgent
 
+
 class QLearningAgent(BaseAgent):
-        
-    def update(self,s,a,r,s_next,done):
+
+    def update(self, s, a, r, s_next, done):
         max_a = np.max(self.Q_sa[s_next])
-        G_t = r + self.gamma * max_a
+        G_t = r if done else r + self.gamma * max_a
         self.Q_sa[s, a] = self.Q_sa[s, a] + self.learning_rate * (G_t - self.Q_sa[s, a])
 
 
-def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True, eval_interval=500):
+def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True,
+               eval_interval=500):
     ''' runs a single repetition of q_learning
-    Return: rewards, a vector with the observed rewards at each timestep ''' 
-    
+    Return: rewards, a vector with the observed rewards at each timestep '''
+
     env = StochasticWindyGridworld(initialize_model=False)
     eval_env = StochasticWindyGridworld(initialize_model=True)
     agent = QLearningAgent(env.n_states, env.n_actions, learning_rate, gamma)
@@ -45,38 +47,40 @@ def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None
             s = env.reset()
         else:
             s = s_next
-    
+
     if plot:
         s = eval_env.reset()
         for t in range(n_timesteps):
             a = agent.select_action(s, policy="greedy")  # sample random action
             s_next, r, done = eval_env.step(a)  # execute action in the environment
 
-            eval_env.render(Q_sa=agent.Q_sa,plot_optimal_policy=True,step_pause=0.5)  # display the environment
+            eval_env.render(Q_sa=agent.Q_sa, plot_optimal_policy=True, step_pause=0.5)  # display the environment
 
             if done:
                 s = eval_env.reset()
             else:
                 s = s_next
 
-    return np.array(eval_returns), np.array(eval_timesteps)   
+    return np.array(eval_returns), np.array(eval_timesteps)
+
 
 def test():
-    
     n_timesteps = 1000
     eval_interval = 100
     gamma = 1.0
     learning_rate = 0.1
 
     # Exploration
-    policy = 'softmax' # 'egreedy' or 'softmax' 
+    policy = 'softmax'  # 'egreedy' or 'softmax'
     epsilon = 0.1
     temp = 1.0
-    
+
     # Plotting parameters
     plot = True
 
-    eval_returns, eval_timesteps = q_learning(n_timesteps, learning_rate, gamma, policy, epsilon, temp, plot, eval_interval)
+    eval_returns, eval_timesteps = q_learning(n_timesteps, learning_rate, gamma, policy, epsilon, temp, plot,
+                                              eval_interval)
+
 
 if __name__ == '__main__':
     test()
